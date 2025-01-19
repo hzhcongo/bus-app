a1b81af2425cc10b098ac67f271b02e0d3d65027
Ivan Heng Ze Hao <ivanhengzehao@gmail.com>
2025-01-19 23:56:40 +0800

    pulled 2 bustops

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,48 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  let MINUTE_MS = 0;
+
+  const [data_62251, setData_62251] = useState(null);
+  const [data_64401, setData_64401] = useState(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch("https://arrivelah2.busrouter.sg/?id=62251")
+        .then((response) => response.json())
+        .then((json) => setData_62251(json))
+        .catch((error) => console.error(error));
+      fetch("https://arrivelah2.busrouter.sg/?id=64401")
+        .then((response) => response.json())
+        .then((json) => setData_64401(json))
+        .catch((error) => console.error(error));
+      MINUTE_MS = 60000;
+    }, MINUTE_MS);
+
+    return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+          <li>
+            {data_62251 ? (
+              <pre>{JSON.stringify(data_62251, null, 2)}</pre>
+            ) : (
+              "Loading..."
+            )}
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>
+            {data_64401 ? (
+              <pre>{JSON.stringify(data_64401, null, 2)}</pre>
+            ) : (
+              "Loading..."
+            )}
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
